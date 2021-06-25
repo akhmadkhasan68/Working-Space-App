f162cfe7f327d12f4241d40edea2fd238400daba
hasan <akhmadkhasan68@gmail.com>
2021-06-25 23:35:43 +0700

    operational page

--- application/views/owner/myworkingspace/partials/operational.php
+++ application/views/owner/myworkingspace/partials/operational.php
@@ -7,9 +7,212 @@
 			<div class="col-md-12">
 				<div class="alert alert-info">
 					<b>Info!</b>
-					Halaman ini digunakan untuk menyesuaikan data fasilitas dari Co-Working Space anda. 
+					Halaman ini digunakan untuk menyesuaikan data jam operasional dari Co-Working Space anda. 
 				</div>
 			</div>
 		</div>
+
+        <div class="row">
+            <div class="col-md-12">
+            <table class="table table-bordered">
+                    <tr>
+                        <th><strong class="text-capitalize"> minggu</th>
+                        <td>
+                            <div class="custom-control custom-checkbox mb-3" id="mingguCheckBoxSection">
+                                <input class="custom-control-input" id="mingguCheckBox" type="checkbox">
+                                <label class="custom-control-label" for="mingguCheckBox">Centang jika hari ini aktif</label>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group form-group-alternative">
+                                        <div class="input-group input-group-alternative mb-4">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="fa fa-clock-o"></i></span>
+                                            </div>
+                                            <input type="text" class="form-control form-control-alternative" id="exampleFormControlInput1" placeholder="Masukkan jam buka">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group form-group-alternative">
+                                        <div class="input-group input-group-alternative mb-4">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="fa fa-clock-o"></i></span>
+                                            </div>
+                                            <input type="text" class="form-control form-control-alternative" id="exampleFormControlInput1" placeholder="Masukkan jam tutup">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th><strong class="text-capitalize"> senin</th>
+                        <td>
+                            <div class="custom-control custom-checkbox mb-3" id="seninCheckBoxSection">
+                                <input class="custom-control-input" id="seninCheckBox" type="checkbox">
+                                <label class="custom-control-label" for="seninCheckBox">Centang jika hari ini aktif</label>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group form-group-alternative">
+                                        <div class="input-group input-group-alternative mb-4">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="fa fa-clock-o"></i></span>
+                                            </div>
+                                            <input type="text" class="form-control form-control-alternative" id="exampleFormControlInput1" placeholder="Masukkan jam buka">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group form-group-alternative">
+                                        <div class="input-group input-group-alternative mb-4">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="fa fa-clock-o"></i></span>
+                                            </div>
+                                            <input type="text" class="form-control form-control-alternative" id="exampleFormControlInput1" placeholder="Masukkan jam tutup">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th><strong class="text-capitalize"> selasa</th>
+                        <td>
+                            <div class="custom-control custom-checkbox mb-3" id="selasaCheckBoxSection">
+                                <input class="custom-control-input" id="selasaCheckBox" type="checkbox">
+                                <label class="custom-control-label" for="selasaCheckBox">Centang jika hari ini aktif</label>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group form-group-alternative">
+                                        <div class="input-group input-group-alternative mb-4">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="fa fa-clock-o"></i></span>
+                                            </div>
+                                            <input type="text" class="form-control form-control-alternative" id="exampleFormControlInput1" placeholder="Masukkan jam buka">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group form-group-alternative">
+                                        <div class="input-group input-group-alternative mb-4">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="fa fa-clock-o"></i></span>
+                                            </div>
+                                            <input type="text" class="form-control form-control-alternative" id="exampleFormControlInput1" placeholder="Masukkan jam tutup">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th><strong class="text-capitalize"> rabu</th>
+                        <td>
+                            <div class="custom-control custom-checkbox mb-3" id="rabuCheckBoxSection">
+                                <input class="custom-control-input" id="rabuCheckBox" type="checkbox">
+                                <label class="custom-control-label" for="rabuCheckBox">Centang jika hari ini aktif</label>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group form-group-alternative">
+                                        <div class="input-group input-group-alternative mb-4">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="fa fa-clock-o"></i></span>
+                                            </div>
+                                            <input type="text" class="form-control form-control-alternative" id="exampleFormControlInput1" placeholder="Masukkan jam buka">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group form-group-alternative">
+                                        <div class="input-group input-group-alternative mb-4">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="fa fa-clock-o"></i></span>
+                                            </div>
+                                            <input type="text" class="form-control form-control-alternative" id="exampleFormControlInput1" placeholder="Masukkan jam tutup">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th><strong class="text-capitalize"> kamis</th>
+                        <td>
+                            <div class="custom-control custom-checkbox mb-3" id="kamisCheckBoxSection">
+                                <input class="custom-control-input" id="kamisCheckBox" type="checkbox">
+                                <label class="custom-control-label" for="kamisCheckBox">Centang jika hari ini aktif</label>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th><strong class="text-capitalize"> jum'at</th>
+                        <td>
+                            <div class="custom-control custom-checkbox mb-3" id="jumatCheckBoxSection">
+                                <input class="custom-control-input" id="jumatCheckBox" type="checkbox">
+                                <label class="custom-control-label" for="jumatCheckBox">Centang jika hari ini aktif</label>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group form-group-alternative">
+                                        <div class="input-group input-group-alternative mb-4">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="fa fa-clock-o"></i></span>
+                                            </div>
+                                            <input type="text" class="form-control form-control-alternative" id="exampleFormControlInput1" placeholder="Masukkan jam buka">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group form-group-alternative">
+                                        <div class="input-group input-group-alternative mb-4">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="fa fa-clock-o"></i></span>
+                                            </div>
+                                            <input type="text" class="form-control form-control-alternative" id="exampleFormControlInput1" placeholder="Masukkan jam tutup">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                    <tr>
+                        <th><strong class="text-capitalize"> sabtu</th>
+                        <td>
+                            <div class="custom-control custom-checkbox mb-3" id="sabtuCheckBoxSection">
+                                <input class="custom-control-input" id="sabtuCheckBox" type="checkbox">
+                                <label class="custom-control-label" for="sabtuCheckBox">Centang jika hari ini aktif</label>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group form-group-alternative">
+                                        <div class="input-group input-group-alternative mb-4">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="fa fa-clock-o"></i></span>
+                                            </div>
+                                            <input type="text" class="form-control form-control-alternative" id="exampleFormControlInput1" placeholder="Masukkan jam buka">
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group form-group-alternative">
+                                        <div class="input-group input-group-alternative mb-4">
+                                            <div class="input-group-prepend">
+                                                <span class="input-group-text"><i class="fa fa-clock-o"></i></span>
+                                            </div>
+                                            <input type="text" class="form-control form-control-alternative" id="exampleFormControlInput1" placeholder="Masukkan jam tutup">
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </td>
+                    </tr>
+                </table>
+            </div>
+        </div>
     </div>
 </div>
