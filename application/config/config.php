d8109fefe481a31a9a222b2ee1380b8c6a3d1cbf
abiyuhuda <86847573+abiyuhuda@users.noreply.github.com>
2021-07-27 14:08:27 +0700

    Update config.php

--- application/config/config.php
+++ application/config/config.php
@@ -26,7 +26,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 $link = (isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] === 'on' ?
                 "https" : "http") . "://" . $_SERVER['HTTP_HOST'];
 
-$config['base_url'] = "$link/workingspace-app";
+$config['base_url'] = "http://localhost/Working-Space-App";
 
 /*
 |--------------------------------------------------------------------------
