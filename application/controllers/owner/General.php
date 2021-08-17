b3e8688a9be2ee0a330789d37a1e7e916740ea7e
hasan <akhmadkhasan68@gmail.com>
2021-08-17 23:30:46 +0700

    delete validator upload photo size

--- application/controllers/owner/General.php
+++ application/controllers/owner/General.php
@@ -18,9 +18,9 @@
         {
             $config['upload_path'] = 'uploads/photos/';
             $config['allowed_types'] = 'gif|jpg|png|jpeg';
-            $config['max_size']  = '1000';
-            $config['max_width']  = '1024';
-            $config['max_height']  = '768';
+            // $config['max_size']  = '1000';
+            // $config['max_width']  = '1024';
+            // $config['max_height']  = '768';
             $config['encrypt_name'] = true;
 
             $this->upload->initialize($config);
